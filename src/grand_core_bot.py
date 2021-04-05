925ee68447890515f0b0d44aee9801e81c07a33d
Ваше Имя <you@example.com>
2021-04-05 19:07:47 -0300

    microfix

--- src/grand_core_bot.py
+++ src/grand_core_bot.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Howdy222, how are you doing?")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
