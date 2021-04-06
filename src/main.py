749fe7dfc7e95560413c4ea475c33675d37abf7d
Ваше Имя <you@example.com>
2021-04-06 00:00:28 -0300

    microfix

--- src/main.py
+++ src/main.py
@@ -6,10 +6,10 @@ import telebot
 print (settings.tg_token)
 
 
-bot = telebot.TeleBot(settings.tg_token)
+#bot = telebot.TeleBot(settings.tg_token)
 
-@bot.message_handler(content_types=['text'])
-def lalala(message):
-    bot.send_message(message.chat.id, message.text)
+#@bot.message_handler(content_types=['text'])
+#def lalala(message):
+#    bot.send_message(message.chat.id, message.text)
 
-bot.polling(none_stop=True)
+#bot.polling(none_stop=True)
