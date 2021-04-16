97ac920b2fbd5069a87ac9287b1445297453d6bf
Ваше Имя <you@example.com>
2021-04-16 16:01:09 -0300

    microfix

--- default/asgi.py
+++ default/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for default project.
+ASGI config for default2 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'default.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'default2.settings')
 
 application = get_asgi_application()
